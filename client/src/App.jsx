f7a98ad102317ff685348f5d66b908c79762c38c
歐陽兆國 <deat@Lay-All-Day.local>
2023-04-18 17:41:17 +0800

    mui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
-
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 function App() {
-  return <div className="App">App</div>
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          src="https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI"
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  )
 }
 
 export default App
